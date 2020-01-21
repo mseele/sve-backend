8e5575a8b911c0dbf1db85253e3e19bc08c5fb6a
mseele <mseele@gmail.com>
2020-01-21 22:10:31 +0100

    remove test email

--- src/main/java/de/sve/backend/api/Test.java
+++ src/main/java/de/sve/backend/api/Test.java
@@ -5,21 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import de.sve.backend.mail.Mail;
-import de.sve.backend.mail.MailAccount;
-
 @Path("test")
 public class Test {
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getMessage() {
-		boolean result = Mail.via(MailAccount.FITNESS)
-			.to("mseele@gmail.com")
-			.bcc(MailAccount.FITNESS.email())
-			.subject("Cool, funktioniert")
-			.content("Ja, es tut :)").send();
-		return "Email send: " + result; //$NON-NLS-1$
+		return "test"; //$NON-NLS-1$
 	}
 
 }
