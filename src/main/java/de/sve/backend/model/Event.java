ebce480ec03ad3148aa6a607b64b991dcbfc055f
mseele <mseele@gmail.com>
2020-01-27 17:48:36 +0100

    add creators

--- src/main/java/de/sve/backend/model/Event.java
+++ src/main/java/de/sve/backend/model/Event.java
@@ -1,7 +1,11 @@
 package de.sve.backend.model;
 
+import static com.google.common.base.MoreObjects.firstNonNull;
+
 import java.time.LocalDateTime;
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 import com.google.auto.value.AutoValue;
 import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
@@ -10,50 +14,105 @@ import com.ryanharter.auto.value.gson.GenerateTypeAdapter;
 @GenerateTypeAdapter
 public abstract class Event {
 
+	public static Event create(String id, String sheetId, Long gid, EventType type, String shortName, String name, Long sortIndex, Boolean visible, String shortDescription, String description,
+			String image, String titleColor, List<LocalDateTime> dates, Long durationInMinutes, Long maxSubscribers, Long subscribers, Double costMember, Double costNonMember, Long waitingList,
+			Long maxWaitingList, String location, String bookingTemplate, String waitingTemplate) {
+		return new AutoValue_Event(id, sheetId, gid, type, shortName, name, sortIndex, visible, shortDescription, description, image, titleColor, dates, durationInMinutes, maxSubscribers, subscribers,
+				costMember, costNonMember, waitingList, maxWaitingList, location, bookingTemplate, waitingTemplate);
+	}
+
 	public abstract String id();
 
+	@Nullable
 	public abstract String sheetId();
 
+	@Nullable
 	public abstract Long gid();
 
+	@Nullable
 	public abstract EventType type();
 
+	@Nullable
 	public abstract String shortName();
 
+	@Nullable
 	public abstract String name();
 
+	@Nullable
 	public abstract Long sortIndex();
 
+	@Nullable
 	public abstract Boolean visible();
 
+	@Nullable
 	public abstract String shortDescription();
 
+	@Nullable
 	public abstract String description();
 
+	@Nullable
 	public abstract String image();
 
+	@Nullable
 	public abstract String titleColor();
 
+	@Nullable
 	public abstract List<LocalDateTime> dates();
 
-	public abstract Integer durationInMinutes();
+	@Nullable
+	public abstract Long durationInMinutes();
 
-	public abstract Integer maxSubscribers();
+	@Nullable
+	public abstract Long maxSubscribers();
 
-	public abstract Integer subscribers();
+	@Nullable
+	public abstract Long subscribers();
 
+	@Nullable
 	public abstract Double costMember();
 
+	@Nullable
 	public abstract Double costNonMember();
 
-	public abstract Integer waitingList();
+	@Nullable
+	public abstract Long waitingList();
 
-	public abstract Integer maxWaitingList();
+	@Nullable
+	public abstract Long maxWaitingList();
 
+	@Nullable
 	public abstract String location();
 
+	@Nullable
 	public abstract String bookingTemplate();
 
+	@Nullable
 	public abstract String waitingTemplate();
+
+	public Event update(Event event) {
+		return create(id(),
+					  firstNonNull(event.sheetId(), sheetId()),
+					  firstNonNull(event.gid(), gid()),
+					  firstNonNull(event.type(), type()),
+					  firstNonNull(event.shortName(), shortName()),
+					  firstNonNull(event.name(), name()),
+					  firstNonNull(event.sortIndex(), sortIndex()),
+					  firstNonNull(event.visible(), visible()),
+					  firstNonNull(event.shortDescription(), shortDescription()),
+					  firstNonNull(event.description(), description()),
+					  firstNonNull(event.image(), image()),
+					  firstNonNull(event.titleColor(), titleColor()),
+					  firstNonNull(event.dates(), dates()),
+					  firstNonNull(event.durationInMinutes(), durationInMinutes()),
+					  firstNonNull(event.maxSubscribers(), maxSubscribers()),
+					  firstNonNull(event.subscribers(), subscribers()),
+					  firstNonNull(event.costMember(), costMember()),
+					  firstNonNull(event.costNonMember(), costNonMember()),
+					  firstNonNull(event.waitingList(), waitingList()),
+					  firstNonNull(event.maxWaitingList(), maxWaitingList()),
+					  firstNonNull(event.location(), location()),
+					  firstNonNull(event.bookingTemplate(), bookingTemplate()),
+					  firstNonNull(event.waitingTemplate(), waitingTemplate()));
+	}
 
 }
