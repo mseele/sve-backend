908acc62b9fca80d4b889941e49b42473da30d29
mseele <mseele@gmail.com>
2020-02-29 10:37:57 +0100

    implement sending of contact message

---
+++ src/main/java/de/sve/backend/api/Contact.java
@@ -0,0 +1,34 @@
+package de.sve.backend.api;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.sve.backend.api.utils.BackendException;
+import de.sve.backend.manager.ContactManager;
+import de.sve.backend.model.contact.Message;
+
+@Path("/contact")
+@SuppressWarnings("static-method")
+public class Contact {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Contact.class);
+
+	@Path("/message")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public void message(Message message) throws BackendException {
+		try {
+			ContactManager.message(message);
+		} catch (Throwable t) {
+			String msg = "Error while sending message"; //$NON-NLS-1$
+			LOG.error(msg, t);
+			throw new BackendException(msg, t);
+		}
+	}
+
+}
