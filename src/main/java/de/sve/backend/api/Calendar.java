07ef2f6e76084dd746e48134462d9f6f8547e8ed
mseele <mseele@gmail.com>
2020-08-11 11:38:35 +0200

    feat(calendar): add notifications hook

--- src/main/java/de/sve/backend/api/Calendar.java
+++ src/main/java/de/sve/backend/api/Calendar.java
@@ -3,9 +3,13 @@ package de.sve.backend.api;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,6 +35,14 @@ public class Calendar {
 			LOG.error(message, t);
 			throw new BackendException(message, t);
 		}
+	}
+
+	@Path("/notifications")
+	@POST
+	public Response notifications(@HeaderParam("X-Goog-Channel-Id") String channelID) {
+		LOG.info("Recieved calendar notification for channel id " + channelID); //$NON-NLS-1$
+		// TODO: fire netlify deploy
+		return Response.status(Status.OK).build();
 	}
 
 }
