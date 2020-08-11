0fc16343764ae8c9b06c1939a24f15479d431e52
mseele <mseele@gmail.com>
2020-08-11 20:41:56 +0200

    feat(calendar): add watch/stop calendar api calls

--- src/main/java/de/sve/backend/manager/CalendarManager.java
+++ src/main/java/de/sve/backend/manager/CalendarManager.java
@@ -11,6 +11,15 @@ public class CalendarManager {
 
 	private static final String GENERAL_ID = "info@sv-eutingen.de"; //$NON-NLS-1$
 
+//	CalendarService.watch(GENERAL_ID, "01234567-89ab-cdef-0123456789ab", LocalDateTime.of(2030, 1, 1, 0, 0));
+//	{
+//	   "expiration":"1599763064000",
+//	   "id":"01234567-89ab-cdef-0123456789ab",
+//	   "kind":"api#channel",
+//	   "resourceId":"9-xc9GFSc2LvPpsJiw8HveIDA3c",
+//	   "resourceUri":"https://www.googleapis.com/calendar/v3/calendars/info@sv-eutingen.de/events?alt=json"
+//	}
+
 	public static List<Appointment> appointments() throws GeneralSecurityException, IOException {
 		return CalendarService.appointments(GENERAL_ID, 100);
 	}
