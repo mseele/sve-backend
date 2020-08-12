d3593bf97cb9678bd550fccc62ac4f089004599c
mseele <mseele@gmail.com>
2020-08-12 21:13:43 +0200

    feat(calendar): trigger re-deploy on calendar change

--- src/main/java/de/sve/backend/api/Calendar.java
+++ src/main/java/de/sve/backend/api/Calendar.java
@@ -1,5 +1,10 @@
 package de.sve.backend.api;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -24,6 +29,8 @@ public class Calendar {
 
 	private static final Logger LOG = LoggerFactory.getLogger(Calendar.class);
 
+	private static final String RE_DEPLOY_HOOK = "https://api.netlify.com/build_hooks/5ede8485bae5450298c17bc4"; //$NON-NLS-1$
+
 	@Path("/appointments")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -41,8 +48,26 @@ public class Calendar {
 	@POST
 	public Response notifications(@HeaderParam("X-Goog-Channel-Id") String channelID) {
 		LOG.info("Recieved calendar notification for channel id " + channelID); //$NON-NLS-1$
-		// TODO: fire netlify deploy
+		try {
+			int statusCode = triggerReDeploy();
+			if (statusCode == 200) {
+				LOG.info("Re-Deploy triggered successfully"); //$NON-NLS-1$
+			} else {
+				LOG.warn("Trigger Re-Deploy failed with status code " + statusCode); //$NON-NLS-1$
+			}
+		} catch (IOException | InterruptedException e) {
+			LOG.error("Trigger Re-Deploy failed", e); //$NON-NLS-1$
+		}
 		return Response.status(Status.OK).build();
+	}
+
+	private static int triggerReDeploy() throws IOException, InterruptedException {
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+										 .uri(URI.create(RE_DEPLOY_HOOK))
+										 .POST(HttpRequest.BodyPublishers.ofString("")) //$NON-NLS-1$
+										 .build();
+		return client.send(request, BodyHandlers.ofString()).statusCode();
 	}
 
 }
