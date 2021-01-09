f4300e293a3496670cd3bf13699bc52a47c260f3
mseele <mseele@gmail.com>
2021-01-09 17:49:29 +0100

    feat(tasks): add renew calendar watch task

--- src/main/java/de/sve/backend/manager/CalendarManager.java
+++ src/main/java/de/sve/backend/manager/CalendarManager.java
@@ -2,7 +2,10 @@ package de.sve.backend.manager;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.time.LocalDateTime;
 import java.util.List;
+
+import com.google.api.services.calendar.model.Channel;
 
 import de.sve.backend.calendar.CalendarService;
 import de.sve.backend.model.calendar.Appointment;
@@ -11,17 +14,18 @@ public class CalendarManager {
 
 	private static final String GENERAL_ID = "info@sv-eutingen.de"; //$NON-NLS-1$
 
-//	CalendarService.watch(GENERAL_ID, "01234567-89ab-cdef-0123456789ab", LocalDateTime.of(2030, 1, 1, 0, 0));
-//	{
-//	   "expiration":"1599763064000",
-//	   "id":"01234567-89ab-cdef-0123456789ab",
-//	   "kind":"api#channel",
-//	   "resourceId":"9-xc9GFSc2LvPpsJiw8HveIDA3c",
-//	   "resourceUri":"https://www.googleapis.com/calendar/v3/calendars/info@sv-eutingen.de/events?alt=json"
-//	}
+	private static final String WATCH_ID = "01234567-89ab-cdef-0123456789ab"; //$NON-NLS-1$
+
+	private static final String WATCH_RESOURCE_ID = "9-xc9GFSc2LvPpsJiw8HveIDA3c"; //$NON-NLS-1$
+
 
 	public static List<Appointment> appointments() throws GeneralSecurityException, IOException {
 		return CalendarService.appointments(GENERAL_ID, 100);
+	}
+
+	public static Channel renewWatch() throws IOException, GeneralSecurityException {
+		CalendarService.stop(WATCH_ID, WATCH_RESOURCE_ID);
+		return CalendarService.watch(GENERAL_ID, WATCH_ID, LocalDateTime.now().plusYears(1));
 	}
 
 }
