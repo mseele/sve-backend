3582470f0fb8d061a084c4967f56386027052003
Michael Seele <mseele@gmail.com>
2022-01-16 21:02:48 +0100

    chore: add actix as server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, SVE!");
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new())
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
