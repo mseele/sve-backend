d93130eb5205b35016194355359d9f9c414173c8
Michael Seele <mseele@gmail.com>
2022-02-23 17:29:13 +0100

    refactor: move response error into api module

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,1 +1,61 @@
 pub mod events;
+
+use actix_web::{error, HttpResponseBuilder};
+use actix_web::{http::header, http::StatusCode, HttpResponse};
+use std::error::Error;
+use std::fmt::{Debug, Display};
+
+pub struct ResponseError {
+    err: anyhow::Error,
+}
+
+impl Error for ResponseError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        self.err.source()
+    }
+
+    //TODO: what to do with backtrace
+    // fn backtrace(&self) -> Option<&std::backtrace::Backtrace> {
+    //    self.err.backtrace()
+    // }
+
+    fn description(&self) -> &str {
+        #[allow(deprecated)]
+        self.err.description()
+    }
+
+    fn cause(&self) -> Option<&dyn Error> {
+        #[allow(deprecated)]
+        self.err.cause()
+    }
+}
+
+impl Debug for ResponseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.err)
+    }
+}
+
+impl Display for ResponseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.err)
+    }
+}
+
+impl From<anyhow::Error> for ResponseError {
+    fn from(err: anyhow::Error) -> ResponseError {
+        ResponseError { err }
+    }
+}
+
+impl error::ResponseError for ResponseError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponseBuilder::new(self.status_code())
+            .insert_header(header::ContentType(mime::TEXT_PLAIN_UTF_8))
+            .body("An internal error occurred. Please try again later.")
+    }
+
+    fn status_code(&self) -> StatusCode {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+}
